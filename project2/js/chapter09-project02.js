86936c2b6cb2c4205c3e75397df8e27841f0b816
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-02-01 13:36:07 -0500

    Step 4 Project 2- Mouse over and mouse out

--- project2/js/chapter09-project02.js
+++ project2/js/chapter09-project02.js
@@ -1,21 +1,36 @@
 /* add code here */
-
-window.addEventListener("load", function()          
+//Step 2
+window.addEventListener("load", function()          //Load event
 {
     var clickImg= document.querySelectorAll("#thumbnails img");
     for(i=0;i<clickImg.length;i++)
     {
         //Step 3
-        clickImg[i].addEventListener("click",function(event)                    
+        clickImg[i].addEventListener("click",function(event)                    //click event
         {
             var filepath = event.target.src;    
-            var filename = filepath.split("images/small");                       
+            var filename = filepath.split("images/small");                      // split till images/small and extract the filename only  
             document.querySelector("#featured img").src = "images/medium"+filename[1];
             document.querySelector("#featured img").title = event.target.title; 
             document.querySelector("#featured img").alt = event.target.alt;
             var caption = document.querySelector("#featured figcaption");
-            caption.style.opacity = "60%";
             caption.innerHTML = event.target.title;
         })
     }
+
+    //Step 4
+    var HoverImg = document.querySelector("#featured img");
+    HoverImg.addEventListener("mouseover",function(event)                    //mouseover event
+        {
+            var caption = document.querySelector("#featured figcaption");
+            caption.style.opacity = "80%";
+        })
+
+    var ImgOut= document.querySelector("#featured img");
+    HoverImg.addEventListener("mouseout",function(event)                    //mouseout event
+        {
+            var caption = document.querySelector("#featured figcaption");
+            caption.style.opacity = "0%";
+        })
+    
 })
