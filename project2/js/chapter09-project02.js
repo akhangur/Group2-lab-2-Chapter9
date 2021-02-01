5ff93131fe6d0c09519b28d62b2dbcd1e6c54c9e
Ammu Joseph <65633351+ajosep13@users.noreply.github.com>
2021-02-01 11:18:04 -0500

    Add files via upload

--- project2/js/chapter09-project02.js
+++ project2/js/chapter09-project02.js
@@ -1,5 +1,16 @@
 /* add code here */
-window.addEventListener("load",function()
-                        {
-  alert(" from load");
-}
+//Step 2
+window.addEventListener("load", function()
+{
+    // alert("After load");
+    var clickImg= document.querySelectorAll("#thumbnails img");
+    for(i=0;i<clickImg.length;i++)
+    {
+
+      var clickEvent=  clickImg.addEventListener("click",function(event)
+    {
+
+    });
+    }
+    
+})
